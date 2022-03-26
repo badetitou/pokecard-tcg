8d4868350d29accee50a5387b87cc9d7ecc1cfbf
badetitou <badetitou@gmail.com>
2022-03-26 21:40:41 +0100

    remove color and add 9th generation

--- lib/pokedex/pokemon.dart
+++ lib/pokedex/pokemon.dart
@@ -1,14 +1,10 @@
 class Pokemon {
   int id;
   Map<String, dynamic> name;
-  List<dynamic> type;
 
-  Pokemon({required this.id, required this.name, required this.type});
+  Pokemon({required this.id, required this.name});
 
   factory Pokemon.fromJson(Map<String, dynamic> parsedJson) {
-    return Pokemon(
-        id: parsedJson['id'],
-        name: parsedJson['name'],
-        type: parsedJson['type']);
+    return Pokemon(id: parsedJson['id'], name: parsedJson['name']);
   }
 }
